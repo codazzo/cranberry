5be76d3541bd4ca1108001ab09972c83c9b31bed
Antonio D'Ettole <codazzo@gmail.com>
2016-03-27 12:29:48 +0200

    Add tests for .catch and .spread

--- test/tests.js
+++ test/tests.js
@@ -138,3 +138,181 @@ describe('.all', function() {
         });
     });
 });
+
+describe('.catch', function() {
+    var promise;
+    var value;
+    var handler;
+
+    describe('when called on a rejected promise', function() {
+        beforeEach(function() {
+            promise = U.reject('woops');
+            value = 'some value';
+            handler = function() {
+                return value;
+            }
+        });
+
+        it('returns the value of the onRejected handler', function() {
+            return promise
+                .catch(handler)
+                .then(function(arg) {
+                    assert.equal(arg, value);
+                });
+        });
+
+        describe('if the onRejected handler returns a fulfilled promise', function() {
+            beforeEach(function() {
+                value = U.resolve('some other value');
+            });
+
+            it('returns a fulfilled promise with the same value as the promise returned by `onRejected`', function() {
+                return promise
+                    .catch(handler)
+                    .then(function(arg) {
+                        assert.equal(arg, 'some other value');
+                    });
+            });
+        });
+
+        describe('if the onRejected handler returns a rejected promise', function() {
+            beforeEach(function() {
+                value = U.reject('nope');
+            });
+
+            it('returns a rejected promise with the same value as the promise returned by `onRejected`', function() {
+                return promise
+                    .catch(handler)
+                    .catch(function(arg) {
+                        assert.equal(arg, 'nope');
+                    });
+            });
+        });
+    });
+
+    describe('when called on a fulfilled promise', function() {
+        var handlerCalled;
+
+        beforeEach(function() {
+            promise = U.resolve('some value');
+            handler = function(){
+                handlerCalled = true;
+            };
+        });
+
+        it('is ignored', function() {
+            return promise
+                .catch(handler)
+                .then(function(){
+                    assert(!handlerCalled);
+                });
+        });
+    });
+
+    describe('when the first argument is a function', function() {
+        var Constructor;
+
+        beforeEach(function() {
+            Constructor = TypeError;
+        });
+
+        describe('if called on a promise whose rejection value is an instance of the function', function() {
+            beforeEach(function() {
+                promise = U.reject(new Constructor());
+            });
+
+            it('uses the second argument as an `onRejected` handler', function() {
+                return promise
+                    .catch(Constructor, function(arg) {
+                        assert.instanceOf(arg, Constructor);
+                    });
+            });
+        });
+
+        describe('if called on a promise whose rejection value is not an instance of the function', function() {
+            beforeEach(function() {
+                promise = U.reject(new Error());
+            });
+
+            it('is not run', function() {
+                var called;
+
+                return promise
+                    .catch(Constructor, function() {
+                        called = true;
+                    })
+                    .catch(function() {
+                        assert(!called)
+                    });
+            });
+        });
+    });
+
+    describe('if called with more than two arguments', function() {
+        it('throws a TypeError', function() {
+            try {
+                promise.catch(Error, function(){}, 'someOtherArg')
+            } catch (e) {
+                assert.instanceOf(e, TypeError);
+            }
+        });
+    });
+});
+
+describe('.spread', function() {
+    var promise;
+
+    describe('if called on a rejected promise', function() {
+        beforeEach(function() {
+            promise = U.reject(new Error('some value'));
+        });
+
+        it('is ignored', function() {
+            var called;
+
+            return promise
+                .spread(function(){
+                    called = true;
+                })
+                .catch(function() {
+                    assert(!called)
+                })
+        });
+    });
+
+    describe('if called on a fulfilled promise', function() {
+        var value;
+
+        describe('if the fulfillment value is an array', function() {
+            beforeEach(function() {
+                value = [
+                    1,
+                    2
+                ];
+                promise = U.resolve(value);
+            });
+
+            it('uses it as an arguments array of the `onFulfillment` handler passed as argument', function() {
+                return promise
+                    .spread(function(arg1, arg2){
+                        assert.equal(arg1, 1);
+                        assert.equal(arg2, 2);
+                    });
+            });
+        });
+
+        describe('if the fulfillment value is not an array', function() {
+            beforeEach(function() {
+                value = 1;
+                promise = U.resolve(value);
+            });
+
+            it('behaves exactly like .then', function() {
+                return promise
+                    .spread(function(arg){
+                        assert.equal(arg, 1);
+                    });
+            });
+        });
+    });
+});
