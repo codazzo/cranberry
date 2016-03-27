40d10b1034404678637c2f53c2c0f0e98e36a5a1
Antonio D'Ettole <codazzo@gmail.com>
2016-03-27 12:43:21 +0200

    Hold native promise in additional argument instead of closure
    Advantage: all cranberry promises use same constructor
    Disadvantage: Promise constructor has additional arguments for internal use - a deviation from the spec

--- index.js
+++ index.js
@@ -7,85 +7,74 @@
         root.U = factory();
     }
 }(this, function () {
-    var U;
+    function U (handler, optionalPromise, onFulfilled, onRejected){
+        var promise = optionalPromise ? optionalPromise.then(wrap(onFulfilled), wrap(onRejected)) : new Promise(handler);
+        var _u = this;
 
-    function buildUFromHandlers(promise, onFulfilled, onRejected){
-        var Ctor = UFactory(promise, onFulfilled, onRejected);
-        return new Ctor();
-    }
+        function wrap(func){
+            return typeof func === 'function' ? function() {
+                var res = func.apply(undefined, arguments);
 
-    function UFactory(optionalPromise, onFulfilled, onRejected) {
-        return function U (handler){
-            var promise = optionalPromise ? optionalPromise.then(wrap(onFulfilled), wrap(onRejected)) : new Promise(handler);
-            var _u = this;
-
-            function wrap(func){
-                return typeof func === 'function' ? function() {
-                    var res = func.apply(undefined, arguments);
-
-                    if (res === _u) {
-                        throw new TypeError('promise resolution value can`t be promise itself')
-                    }
-
-                    return res;
-                } : undefined;
-            }
-
-            function _then(onFulfilled, onRejected) {
-                return buildUFromHandlers(promise, onFulfilled, onRejected);
-            }
-
-            function _spread(func) {
-                return buildUFromHandlers(promise, function (arr) {
-                    if (!Array.isArray(arr)) {
-                        return func.call(null, arr);
-                    }
-                    return func.apply(null, arr);
-                });
-            }
-
-            function _catch(){
-                var handler;
-                var constructor;
-
-                switch (arguments.length) {
-                    case 2:
-                        constructor = arguments[0];
-                        handler = arguments[1];
-                        break;
-                    case 1:
-                        handler = arguments[0];
-                        break;
-                    default:
-                        throw new TypeError('Usage: .catch(constructor, handler) or .catch(handler)');
+                if (res === _u) {
+                    throw new TypeError('promise resolution value can`t be promise itself')
                 }
 
-                return buildUFromHandlers(promise, null, function (val) {
-                    var shouldBeCaught = typeof constructor === 'undefined' || val instanceof constructor;
+                return res;
+            } : undefined;
+        }
 
-                    if (shouldBeCaught) {
-                        return handler.apply(this, arguments);
-                    }
+        function _then(onFulfilled, onRejected) {
+            return new U(null, promise, onFulfilled, onRejected);
+        }
 
-                    throw val;
-                });
-            }
-
-            Object.defineProperties(this, {
-                then: {
-                    value: _then
-                },
-                spread: {
-                    value: _spread
-                },
-                catch: {
-                    value: _catch
+        function _spread(func) {
+            return new U(null, promise, function (arr) {
+                if (!Array.isArray(arr)) {
+                    return func.call(null, arr);
                 }
+                return func.apply(null, arr);
             });
         }
-    }
 
-    U = UFactory();
+        function _catch(){
+            var handler;
+            var constructor;
+
+            switch (arguments.length) {
+                case 2:
+                    constructor = arguments[0];
+                    handler = arguments[1];
+                    break;
+                case 1:
+                    handler = arguments[0];
+                    break;
+                default:
+                    throw new TypeError('Usage: .catch(constructor, handler) or .catch(handler)');
+            }
+
+            return new U(null, promise, null, function (val) {
+                var shouldBeCaught = typeof constructor === 'undefined' || val instanceof constructor;
+
+                if (shouldBeCaught) {
+                    return handler.apply(this, arguments);
+                }
+
+                throw val;
+            });
+        }
+
+        Object.defineProperties(this, {
+            then: {
+                value: _then
+            },
+            spread: {
+                value: _spread
+            },
+            catch: {
+                value: _catch
+            }
+        });
+    }
 
     U.resolve = function resolve(value) {
         return new U(function (resolve) {
